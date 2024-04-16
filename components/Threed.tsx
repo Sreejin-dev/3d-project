b32658548af0b8d2cb2540e4c614786b78c4224f
Sreejin-dev <sreejincp84@gmail.com>
2024-04-16 12:00:46 +0530

    modified:   components/Threed.tsx

--- components/Threed.tsx
+++ components/Threed.tsx
@@ -5,7 +5,7 @@ const Threed = () => {
     <div className="flex px-10  justify-between h-svh items-center">
       <div className=" flex justify-center items-center  w-full h-full">
       <h1 className="text-4xl font-bold  text-[#8d8c8c] mt-10">
-  Step into Style: <br />
+  Step into Style:
   <h1 >Discover the Perfect Pair at our Shoe Haven!</h1> 
 </h1>
       </div>
